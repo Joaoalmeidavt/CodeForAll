efaa551c75fd107b70c429397918a11ed198e099
lhugens <leonardohugens@protonmail.com>
2024-09-21 09:37:41 +0100

    take advantage of static methods

--- algorithms/src/Main.java
+++ algorithms/src/Main.java
@@ -19,13 +19,12 @@ public class Main {
         int toBeFound = 8;
 
         // Linear Search
-        Search ls = new Search();
         System.out.println("Linear Search\n");
         System.out.print("Data: ");
         printArr(data);
 
         System.out.println("Value: " + toBeFound);
-        int index = ls.linearSearch(data, toBeFound);
+        int index = Search.linearSearch(data, toBeFound);
         if(index == -1){
             System.out.println("Element not found");
         } else {
@@ -33,27 +32,24 @@ public class Main {
         }
 
         // Selection Sort
-        Sort ss = new Sort();
         System.out.println("\nSelection Sort\n\nData:");
         printArr(data);
         System.out.println("Sorted:");
-        int[] sorted = ss.selectionSort(data);
+        int[] sorted = Sort.selectionSort(data);
         printArr(sorted);
 
         // Insertion Sort
-        Sort is = new Sort();
         System.out.println("\nInsertion Sort\n\nData:");
         printArr(ogData);
-        sorted = is.insertionSort(ogData);
+        sorted = Sort.insertionSort(ogData);
         System.out.println("Sorted:");
         printArr(sorted);
 
         // Binary Search
-        Search bs = new Search();
         System.out.println("\nBinary Search");
         System.out.println("Data: ");
         printArr(data);
-        index = bs.binarySearch(data, toBeFound);
+        index = Search.binarySearch(data, toBeFound);
         if(index == -1){
             System.out.println("Element not found");
         } else {
@@ -62,20 +58,18 @@ public class Main {
 
         // Factorial
         int n = 4;
-        Recursive fac = new Recursive();
-        int fact = fac.factorial(n);
-        System.out.println("\nFactorial of " + n + " is " + fact);
+        int fact = Recursive.factorial(n);
+        System.out.println("\nFactorial of " + n + " is " + fact + "\n");
 
         // Palindrome
         String word = "ANA";
-        Recursive pal = new Recursive();
-        if(pal.isPalindrome(word)){
+        if(Recursive.isPalindrome(word)){
             System.out.println(word + " is a palindrome.");
         } else {
             System.out.println(word + " is not a palindrome.");
         }
         word = "NOT";
-        if(pal.isPalindrome(word)){
+        if(Recursive.isPalindrome(word)){
             System.out.println(word + " is a palindrome.");
         } else {
             System.out.println(word + " is not a palindrome.");
