21b67373972a61921a2bb1c564b4243d33fd2d6d
Leonardo Hugens <leonardohugens@protonmail.com>
2024-09-23 17:08:42 +0100

    S1Ep1 Ex2: Correction

--- exercises/ex2/NotString.java
+++ exercises/ex2/NotString.java
@@ -1,8 +1,9 @@
 class NotString {
 	public static void main(String[] args){
 		String s = "semicolon";
-		System.out.println((s.contains("not")) ? s : "not " + s);
-		s = "not semicolon";
-		System.out.println((s.contains("not")) ? s : "not " + s);
+		if(args.length > 0){
+			s = args[0];
+		}
+		System.out.println((s.substring(0,3).equals("not")) ? s : "not " + s);
 	}
 }
