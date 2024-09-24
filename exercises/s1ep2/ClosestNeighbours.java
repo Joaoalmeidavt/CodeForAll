6e68f83145698e3fc8fc6d96d938a0397bf95d60
Leonardo Hugens <leonardohugens@protonmail.com>
2024-09-24 17:25:52 +0100

    S1Ep2 ex1

--- exercises/s1ep2/ClosestNeighbours.java
+++ exercises/s1ep2/ClosestNeighbours.java
@@ -3,16 +3,14 @@ class ClosestNeighbours {
 		int[] numbers = {0, 5, 1209, 6, 2, 111, 112, 33};
 		int[] closest = new int[2];
 		int min = 100000000;
-		for(int i=0; i<numbers.length; i++){
-			for(int j=i+1; j<numbers.length; j++){
-				int number1 = numbers[i];
-				int number2 = numbers[j];
-				int distance = Math.abs(number1 - number2);
-				if(distance < min){
-					min = distance;
-					closest[0] = number1;	
-					closest[1] = number2;	
-				}
+		for(int i=0; i<numbers.length-1; i++){
+			int number1 = numbers[i];
+			int number2 = numbers[i+1];
+			int distance = Math.abs(number1 - number2);
+			if(distance < min){
+				min = distance;
+				closest[0] = number1;	
+				closest[1] = number2;	
 			}
 		}
 
