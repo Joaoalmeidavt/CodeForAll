8f0804899372c6eb33b8c57bb25ebb6f8667ca11
Leonardo Hugens <leonardohugens@protonmail.com>
2024-09-25 10:26:48 +0100

    S1Ep2 ex1

--- exercises/s1ep2/ClosestNeighbours.java
+++ exercises/s1ep2/ClosestNeighbours.java
@@ -1,19 +1,16 @@
 class ClosestNeighbours {
-	public static void main(String[] args){
-		int[] numbers = {0, 5, 1209, 6, 2, 111, 112, 33};
-		int[] closest = new int[2];
-		int min = 100000000;
-		for(int i=0; i<numbers.length-1; i++){
-			int number1 = numbers[i];
-			int number2 = numbers[i+1];
-			int distance = Math.abs(number1 - number2);
-			if(distance < min){
-				min = distance;
-				closest[0] = number1;	
-				closest[1] = number2;	
-			}
-		}
-
-		System.out.println("The two closest neighbours are " + closest[0] + " and " + closest[1] + ".");
-	}	
+    public static void main(String[] args) {
+        int[] numbers = {0, 5, 1209, 6, 2, 111, 112, 33};
+        int[] closest = new int[2];
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < numbers.length - 1; i++) {
+            int distance = Math.abs(numbers[i] - numbers[i+1]);
+            if (distance < min) {
+                min = distance;
+                closest[0] = numbers[i];
+                closest[1] = numbers[i+1];
+            }
+        }
+        System.out.println("The two closest neighbours are " + closest[0] + " and " + closest[1] + ".");
+    }
 }
