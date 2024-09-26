8671def8d253f67c094b00b92a1a37dbe67fbdad
Leonardo Hugens <leonardohugens@protonmail.com>
2024-09-26 08:28:03 +0100

    organize

--- exercises/s1ep1/ex1/ex2/NotString.java
+++
@@ -1,9 +0,0 @@
-class NotString {
-	public static void main(String[] args){
-		String s = "semicolon";
-		if(args.length > 0){
-			s = args[0];
-		}
-		System.out.println((s.substring(0,3).equals("not")) ? s : "not " + s);
-	}
-}
