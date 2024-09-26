99b650627742c4a799da880ed334f5915bf56c4c
Leonardo Hugens <leonardohugens@protonmail.com>
2024-09-26 09:16:25 +0100

    organize

--- exercises/s1ep1/ex3/FrontBack.java
+++
@@ -1,9 +0,0 @@
-class FrontBack {
-	public static void main(String[] args){
-		String s = "FrontBack";
-		if(args.length > 0){
-			s = args[0];
-		}
-		System.out.println(s.charAt(s.length()-1) + s.substring(1, s.length()-1) + s.charAt(0));
-	}
-}  
--- exercises/s1ep3/ex1/Sandbox.java
+++
@@ -1,8 +0,0 @@
-public class Sandbox {
-    public static void main(String[] args) {
-        Calculator calc = new Calculator("casio", "silver" );
-        int result = calc.add(1, 2, 3);
-
-        System.out.println(result);
-    }
-}
