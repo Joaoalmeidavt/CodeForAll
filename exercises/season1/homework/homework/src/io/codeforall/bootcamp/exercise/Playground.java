87547e8aadc8950383eccb7d91feaab274467813
lhugens <leonardohugens@protonmail.com>
2024-09-29 10:25:13 +0100

    Season 1 homework - palindromReaplce new attempt, sill failing

--- exercises/season1/homework/homework/src/io/codeforall/bootcamp/exercise/Playground.java
+++ exercises/season1/homework/homework/src/io/codeforall/bootcamp/exercise/Playground.java
@@ -1,22 +1,28 @@
+package io.codeforall.bootcamp.exercise;
 public class Playground {
     public static void main(String[] args) {
-        Ai ai = new Ai("28/09/2024", "Jacob");
+        io.codeforall.bootcamp.exercise.Ai ai = new io.codeforall.bootcamp.exercise.Ai("28/09/2024", "Jacob");
 
         // getCreationDate method example
+        System.out.println("getCreationDate method example");
         System.out.println(ai.getCreationDate());
 
         // setCreationDate method example
+        System.out.println("setCreationDate method example");
         ai.setCreationDate("29/09/2024");
         System.out.println(ai.getCreationDate());
 
         // getName method example
+        System.out.println("getName method example");
         System.out.println(ai.getName());
 
         // setName method example
+        System.out.println("setName method example");
         ai.setName("Tigran");
         System.out.println(ai.getName());
 
         // doubleArray method example
+        System.out.println("doubleArray method example");
         int[] arr = new int[]{1, 2, 3, 4};
         arr = ai.doubleArray(arr);
         for(int element : arr){
@@ -25,9 +31,19 @@ public class Playground {
         System.out.println("");
 
         // factorialIterative method example
+        System.out.println("factorialIterative method example");
         System.out.println(ai.factorialIterative(5));
 
         // factorialRecursive method example
+        System.out.println("factorialRecursive method example");
         System.out.println(ai.factorialRecursive(5));
+
+        // palindromeReplace method example
+        System.out.println("palindromeReplace method example ");
+        System.out.println(ai.palindromeReplace("anotaramadatadamaratona miguel rito"));
+
+        // emailValidator method example
+        System.out.println("emailValidator method example ");
+        System.out.println(ai.emailValidator());
     }
 }
