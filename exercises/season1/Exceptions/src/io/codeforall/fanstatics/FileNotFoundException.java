6c31bac2f6608760e563177185483fcfb150739f
Leonardo Hugens <leonardohugens@protonmail.com>
2024-10-08 14:19:54 +0100

    Season 1 - Exceptions

--- exercises/season1/Exceptions/src/io/codeforall/fanstatics/FileNotFoundException.java
+++ exercises/season1/Exceptions/src/io/codeforall/fanstatics/FileNotFoundException.java
@@ -3,7 +3,6 @@ package io.codeforall.fanstatics;
 public class FileNotFoundException extends FileException {
 
     public FileNotFoundException(){
-        System.out.println("File Not Found exception");
     }
 
     public FileNotFoundException(String s){
