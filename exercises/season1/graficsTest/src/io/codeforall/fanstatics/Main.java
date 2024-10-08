f87e9517b2db594d637bfea2a43b581283c88258
Leonardo Hugens <leonardohugens@protonmail.com>
2024-10-08 17:58:16 +0100

    Season 1 - Simple Graphics Library

---
+++ exercises/season1/graficsTest/src/io/codeforall/fanstatics/Main.java
@@ -0,0 +1,15 @@
+package io.codeforall.fanstatics;
+
+
+import org.academiadecodigo.simplegraphics.graphics.Color;
+import org.academiadecodigo.simplegraphics.graphics.Rectangle;
+
+public class Main {
+    public static void main(String[] args) {
+        Rectangle rect = new Rectangle(0, 0, 400, 400);
+        rect.setColor(Color.BLUE);
+        rect.fill();
+
+        Car can = new Car();
+    }
+}
