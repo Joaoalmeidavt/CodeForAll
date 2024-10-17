835f179fe84b0263a62d29edcb3100f20afb5414
Leonardo Hugens <leonardohugens@protonmail.com>
2024-10-17 11:42:29 +0100

    Season 1 - Paint color black or back to white

---
+++ exercises/season1/Paint/src/io/codeforall/fanstatics/Paint.java
@@ -0,0 +1,110 @@
+package io.codeforall.fanstatics;
+
+
+import io.codeforall.fanstatics.gfx.simplegfx.SimpleGfxGrid;
+import io.codeforall.fanstatics.gfx.simplegfx.SimpleGfxGridPosition;
+import io.codeforall.fanstatics.grid.Grid;
+import io.codeforall.fanstatics.grid.GridColor;
+import io.codeforall.fanstatics.grid.GridDirection;
+import io.codeforall.fanstatics.grid.SimpleGfxGridFactory;
+import org.academiadecodigo.simplegraphics.graphics.Color;
+import org.academiadecodigo.simplegraphics.keyboard.Keyboard;
+import org.academiadecodigo.simplegraphics.keyboard.KeyboardEvent;
+import org.academiadecodigo.simplegraphics.keyboard.KeyboardEventType;
+import org.academiadecodigo.simplegraphics.keyboard.KeyboardHandler;
+
+import java.util.Arrays;
+
+public class Paint implements KeyboardHandler {
+    private int cols;
+    private int rows;
+    private Keyboard keyboard;
+    private SimpleGfxGrid grid;
+    private SimpleGfxGridPosition[][] positions;
+    private boolean[][] blackOn;
+    private SimpleGfxGridPosition cursor;
+
+    public Paint(int cols, int rows) {
+        this.cols = cols;
+        this.rows = rows;
+        this.grid = SimpleGfxGridFactory.makeGrid(cols, rows);
+        this.grid.init();
+        this.positions = new SimpleGfxGridPosition[this.grid.getCols()][this.grid.getRows()];
+        this.blackOn = new boolean[this.grid.getCols()][this.grid.getRows()];
+        for (int i = 0; i < this.grid.getCols(); i++) {
+            for (int j = 0; j < this.grid.getRows(); j++) {
+                this.positions[i][j] = new SimpleGfxGridPosition(i, j, this.grid);
+                this.blackOn[i][j] = false;
+            }
+        }
+        this.cursor = new SimpleGfxGridPosition(0, 0, this.grid);
+        this.cursor.setColor(GridColor.GREEN);
+
+        initKeyboard();
+    }
+
+    private void initKeyboard() {
+        this.keyboard = new Keyboard(this);
+
+        KeyboardEvent moveRight = new KeyboardEvent();
+        moveRight.setKey(KeyboardEvent.KEY_RIGHT);
+        moveRight.setKeyboardEventType(KeyboardEventType.KEY_PRESSED);
+        this.keyboard.addEventListener(moveRight);
+
+        KeyboardEvent moveLeft = new KeyboardEvent();
+        moveLeft.setKey(KeyboardEvent.KEY_LEFT);
+        moveLeft.setKeyboardEventType(KeyboardEventType.KEY_PRESSED);
+        this.keyboard.addEventListener(moveLeft);
+
+        KeyboardEvent moveUp = new KeyboardEvent();
+        moveUp.setKey(KeyboardEvent.KEY_UP);
+        moveUp.setKeyboardEventType(KeyboardEventType.KEY_PRESSED);
+        this.keyboard.addEventListener(moveUp);
+
+        KeyboardEvent moveDown = new KeyboardEvent();
+        moveDown.setKey(KeyboardEvent.KEY_DOWN);
+        moveDown.setKeyboardEventType(KeyboardEventType.KEY_PRESSED);
+        this.keyboard.addEventListener(moveDown);
+
+        KeyboardEvent space = new KeyboardEvent();
+        space.setKey(KeyboardEvent.KEY_SPACE);
+        space.setKeyboardEventType(KeyboardEventType.KEY_PRESSED);
+        this.keyboard.addEventListener(space);
+    }
+
+    @Override
+    public void keyPressed(KeyboardEvent keyboardEvent) {
+        switch (keyboardEvent.getKey()) {
+            case KeyboardEvent.KEY_RIGHT:
+                this.cursor.moveInDirection(GridDirection.RIGHT, 1);
+                break;
+            case KeyboardEvent.KEY_LEFT:
+                this.cursor.moveInDirection(GridDirection.LEFT, 1);
+                break;
+            case KeyboardEvent.KEY_UP:
+                this.cursor.moveInDirection(GridDirection.UP, 1);
+                break;
+            case KeyboardEvent.KEY_DOWN:
+                this.cursor.moveInDirection(GridDirection.DOWN, 1);
+                break;
+            case KeyboardEvent.KEY_SPACE:
+                SimpleGfxGridPosition place = this.positions[this.cursor.getCol()][this.cursor.getRow()];
+                boolean black = this.blackOn[this.cursor.getCol()][this.cursor.getRow()];
+                if(black){
+                    this.blackOn[this.cursor.getCol()][this.cursor.getRow()] = false;
+                    place.rectangle.setColor(Color.WHITE);
+                    place.rectangle.fill();
+                    break;
+                }
+                this.blackOn[this.cursor.getCol()][this.cursor.getRow()] = true;
+                place.rectangle.setColor(Color.BLACK);
+                place.rectangle.fill();
+                break;
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyboardEvent keyboardEvent) {
+
+    }
+}
