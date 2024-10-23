68ba2175de5482588a473bacb96c7a7076a2b32f
Leonardo Hugens <leonardohugens@protonmail.com>
2024-10-23 14:47:46 +0100

    Season 2 - UDP server Client and Server

--- exercises/season2/UDPClientServer/src/io/codeforall/fanstatics/Server.java
+++ exercises/season2/UDPClientServer/src/io/codeforall/fanstatics/Server.java
@@ -3,11 +3,9 @@ package io.codeforall.fanstatics;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
-import java.net.UnknownHostException;
 
-public class Main {
+public class Server {
     public static void main(String[] args) {
-        String hostname = "localhost";
         int port = 50000;
 
         byte[] receiveBuffer = new byte[1024];
