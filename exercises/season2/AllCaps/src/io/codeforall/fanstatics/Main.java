d329fd190db6b5438b78a718a9cafbea0bcd8bd6
Leonardo Hugens <leonardohugens@protonmail.com>
2024-11-04 13:18:10 +0000

    Season 2 - AllCaps

---
+++ exercises/season2/AllCaps/src/io/codeforall/fanstatics/Main.java
@@ -0,0 +1,13 @@
+package io.codeforall.fanstatics;
+
+import java.sql.SQLOutput;
+import java.util.stream.Stream;
+
+public class Main {
+    public static void main(String[] args) {
+        String message = "I'll send an SOS to the garbage world. " +
+                "I hope that someone garbage gets my message in a garbage bottle.";
+
+        System.out.println(String.join(" ", Stream.of(message.split(" ")).map(String::toUpperCase).toList()));
+    }
+}
