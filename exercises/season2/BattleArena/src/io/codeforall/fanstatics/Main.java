ec5f8b2329a43e34859b47239efe0caa9b5293c0
lhugens <leonardohugens@protonmail.com>
2024-11-04 18:10:22 +0000

    test

--- exercises/season2/BattleArena/src/io/codeforall/fanstatics/Main.java
+++ exercises/season2/BattleArena/src/io/codeforall/fanstatics/Main.java
@@ -9,5 +9,6 @@ public class Main {
     public static void main(String[] args) {
         Arena arena = new Arena(new Cleric(), new Mage(), new Rogue(), new Warrior());
         arena.start();
+        //
     }
 }
