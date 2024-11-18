01e90ef4e3478ab1fad730c423599bdb8d099e6b
Leonardo Hugens <leonardohugens@protonmail.com>
2024-11-18 16:08:55 +0000

    Season 4 - Hibernate JPA

---
+++ exercises/season4/HibernateJPA/src/main/java/io/codeforall/fanstatics/Cadet.java
@@ -0,0 +1,42 @@
+package io.codeforall.fanstatics;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "cadet")
+public class Cadet {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private Integer age;
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+}
