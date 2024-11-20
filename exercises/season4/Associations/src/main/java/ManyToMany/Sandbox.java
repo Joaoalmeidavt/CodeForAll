be2204878b7c7c7fd507c4bf81c4a4995556e719
Leonardo Hugens <leonardohugens@protonmail.com>
2024-11-20 08:39:45 +0000

    Season 4 - Assicutations Mapping

--- exercises/season4/Associations/src/main/java/ManyToMany/Sandbox.java
+++ exercises/season4/Associations/src/main/java/ManyToMany/Sandbox.java
@@ -12,18 +12,36 @@ public class Sandbox {
 
         EntityManager em = emf.createEntityManager();
 
-        User user = new User();
-        user.setUsername("CodeCadet");
-        user.setPassword("cenas");
+        User user1 = new User();
+        user1.setUsername("CodeCadet");
+        user1.setPassword("cenas");
 
-        SecurityGroup sGroup = new SecurityGroup();
-        sGroup.addUser(user);
+        User user2 = new User();
+        user2.setUsername("Margret");
+        user2.setPassword("osdfoiwov");
 
-        user.addSgroups(sGroup);
+        User user3 = new User();
+        user3.setUsername("Frida");
+        user3.setPassword("sdf0ivp");
+
+        SecurityGroup sGroup1 = new SecurityGroup();
+        sGroup1.addUser(user1);
+        sGroup1.addUser(user2);
+
+        user1.addSgroups(sGroup1);
+        user2.addSgroups(sGroup1);
+
+        SecurityGroup sGroup2 = new SecurityGroup();
+        sGroup2.addUser(user3);
+
+        user3.addSgroups(sGroup2);
 
         em.getTransaction().begin();
-        em.persist(user);
-        em.persist(sGroup);
+        em.persist(user1);
+        em.persist(user2);
+        em.persist(user3);
+        em.persist(sGroup1);
+        em.persist(sGroup2);
         em.getTransaction().commit();
 
         em.close();
--- exercises/season4/Associations/src/main/java/ManyToOne/Sandbox.java
+++ exercises/season4/Associations/src/main/java/ManyToOne/Sandbox.java
@@ -17,15 +17,24 @@ public class Sandbox {
         Category category = new Category();
         category.setName("Beauty");
 
-        Product product = new Product();
-        product.setCategory(category);
-        product.setName("Blush");
+        Product product1 = new Product();
+        product1.setCategory(category);
+        product1.setName("Blush");
+
+        Product product2 = new Product();
+        product2.setCategory(category);
+        product2.setName("Eyeliner");
+
+        Product product3 = new Product();
+        product3.setCategory(category);
+        product3.setName("Foundation");
 
         em.getTransaction().begin();
         em.persist(category);
-        em.persist(product);
+        em.persist(product1);
+        em.persist(product2);
+        em.persist(product3);
         em.getTransaction().commit();
-
 
         em.close();
         emf.close();
